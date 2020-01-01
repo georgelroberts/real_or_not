46f1ba5955940c5afa0b9c524919c0c4c9abb67b
George <georgelroberts@hotmail.com>
2020-01-01 17:24:43 +0000

    Created a data extraction class
    New data extraction class will make it easier to clean+use data across exploratory data analysis and model fitting.

--- exploration.py
+++ exploration.py
@@ -5,14 +5,16 @@ Description: A quick analysis of the train/test dataset.
 Ideas:
     Hashtags should be more important features.
     Most entries have a 'keyword', but only ~2/3 have a location
+    Show length of tweets, any correlation with classification
+    Number of hashtags as a feature
+    Caps locks/punctuation could be important.
+    Using location will almost certainly lead to overfitting.
 """
 
 import os
 import numpy as np
 import pandas as pd
-
-CDIR = os.path.abspath(os.path.dirname(__file__))
-DATA_DIR = os.path.join(CDIR, 'data')
+from extract_data import Extract_Data
 
 
 def main():
@@ -20,8 +22,8 @@ def main():
 
 
 def explore_data():
-    train = load_dataset('train')
-    test = load_dataset('test')
+    data_extractor = Extract_Data()
+    train, test = data_extractor.load_data()
     print('Starting an exploratory data analysis.')
     print('Please see the README.md for more information\n')
     print('Train dataset information:')
@@ -34,17 +36,6 @@ def explore_data():
     print(train.head(3))
     print('First 3 test rows:')
     print(test.head(3))
-
-
-def load_dataset(dset='train'):
-    """
-    Args:
-        dset (str): train or test, default train
-    Returns:
-        pd.Dataframe: desired dataset
-    """
-    fname = os.path.join(DATA_DIR, f'{dset}.csv')
-    return pd.read_csv(fname)
 
 
 if __name__ == '__main__':
---
+++ extract_data.py
@@ -0,0 +1,22 @@
+"""
+Author: G. L. Roberts
+Date: 01-01-2020
+Description: A class to extract/clean datasets
+"""
+
+import os
+import pandas as pd
+
+class Extract_Data(object):
+    def __init__(self):
+        self.cdir = os.path.abspath(os.path.dirname(__file__))
+        self.data_dir = os.path.join(self.cdir, 'data')
+        self.train_fname = os.path.join(self.data_dir, 'train.csv')
+        self.test_fname = os.path.join(self.data_dir, 'test.csv')
+    
+    def load_data(self):
+        """ Return pd DataFrame of train and test """
+        self.train = pd.read_csv(self.train_fname)
+        self.test = pd.read_csv(self.test_fname)
+        return self.train, self.test
+
