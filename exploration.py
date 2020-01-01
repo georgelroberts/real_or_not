436c8516229923a01c9165ad9d806bd9a606d924
George <georgelroberts@hotmail.com>
2020-01-01 16:31:32 +0000

    Added initial data exploration code, only loading data

---
+++ exploration.py
@@ -0,0 +1,32 @@
+"""
+Author: G. L. Roberts
+Date: 01-01-2020
+Description: A quick analysis of the train/test dataset.
+"""
+
+import os
+import numpy as np
+import pandas as pd
+
+CDIR = os.path.abspath(os.path.dirname(__file__))
+DATA_DIR = os.path.join(CDIR, 'data')
+
+
+def main():
+    train = load_dataset('train')
+    test = load_dataset('test')
+
+
+def load_dataset(dset='train'):
+    """
+    Args:
+        dset (str): train or test, default train
+    Returns:
+        pd.Dataframe: desired dataset
+    """
+    fname = os.path.join(DATA_DIR, f'{dset}.csv')
+    return pd.read_csv(fname)
+
+
+if __name__ == '__main__':
+    main()
