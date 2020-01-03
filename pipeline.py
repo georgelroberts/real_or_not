2d1b059f2e94a5f216b0bfe53d97ca7e3e024dae
George <georgelroberts@hotmail.com>
2020-01-03 18:05:20 +0000

    Create pipeline.py
    Dummy file for complete model pipeline.

---
+++ pipeline.py
@@ -0,0 +1,14 @@
+"""
+Author: G. L. Roberts
+Date: 03-01-2020
+About: Complete pipeline for extracting data, fitting and scoring
+"""
+
+from extract_data import Extract_Data
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
