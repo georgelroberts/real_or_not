ae37ae4f37076b75fd77c5a1a6bc1ee06e1df98a
George <georgelroberts@hotmail.com>
2020-01-03 18:43:53 +0000

    Update pipeline.py
    Fit a preliminary Multinomial Naive Bayes model and scored using F1

--- pipeline.py
+++ pipeline.py
@@ -6,12 +6,25 @@ About: Complete pipeline for extracting data, fitting and scoring
 
 from extract_data import Extract_Data
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import f1_score
 
 def main():
+    pipeline()
+
+def pipeline():
     data_extractor = Extract_Data()
     train_X, train_y, test_X = data_extractor.get_train_test()
     train_X, train_y, cv_X, cv_y = split_train_cv(train_X, train_y)
-    breakpoint()
+    clf = MultinomialNB()
+    clf.fit(train_X, train_y)
+    train_pred = clf.predict(train_X)
+    train_f1 = f1_score(train_y, train_pred)
+    print(f'Train F1 score: {train_f1:.4f}')
+    cv_pred = clf.predict(cv_X)
+    cv_f1 = f1_score(cv_y, cv_pred)
+    print(f'CV F1 score: {cv_f1:.4f}')
+    return
 
 
 def split_train_cv(train_X, train_y):
@@ -19,7 +32,6 @@ def split_train_cv(train_X, train_y):
         train_X, train_y, test_size=0.33, random_state=42)
     return train_X, train_y, cv_X, cv_y
     
-
 
 if __name__ == '__main__':
     main()
