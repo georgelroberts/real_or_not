5c33ab8fee84fdebbfb4c7d7b09c9935c38aa636
George <georgelroberts@hotmail.com>
2020-01-04 20:45:50 +0000

    Create scoring.py
    Created a class to deal with the scoring

---
+++ scoring.py
@@ -0,0 +1,18 @@
+"""
+Author: G. L. Roberts
+Date: 04-01-2020
+About: Class to add scores and to view scores to a dataframe
+"""
+
+class Scoring(object):
+    def __init__(self):
+        pass
+
+    def add_to_df(self):
+        pass
+
+    def check_if_in_df(self):
+        pass
+
+    def print_best_score(self):
+        pass
