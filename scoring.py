627477b2f8fbe8f0262bdd0117351b92cce14ee5
George <georgelroberts@hotmail.com>
2020-01-05 08:18:54 +0000

    Update scoring.py
    Added comments to scoring

--- scoring.py
+++ scoring.py
@@ -20,6 +20,7 @@ class Scoring(object):
         self.load_df()
 
     def load_df(self):
+        """ Load pickled dataframe on disk if it exists, else make it """
         if not os.path.exists(self.results_fpath):
             self.make_blank_df()
         else:
@@ -27,14 +28,17 @@ class Scoring(object):
                 self.score_df = pickle.load(f)
     
     def make_blank_df(self):
+        """ Initialise a blank dataframe and save it on disk """
         self.score_df = pd.DataFrame(columns=['model_name', 'cv_f1', 'train_f1'])
         self.save_df()
 
     def save_df(self):
+        """ Save dataframe on disk """
         with open(self.results_fpath, 'wb') as f:
             pickle.dump(self.score_df, f)
 
     def add_scores(self, name, cv_score, train_score):
+        """ Add scores to the dataframe and save it """
         is_in = self.check_if_in_df(name)
         if not is_in:
             position = len(self.score_df)
@@ -42,6 +46,7 @@ class Scoring(object):
             self.save_df()
 
     def check_if_in_df(self, name):
+        """ Check if model name already exists in the dataframe """
         if self.score_df['model_name'].str.contains(name).sum() > 0:
             print('Model already in dataframe')
             return True
@@ -49,7 +54,9 @@ class Scoring(object):
             return False
 
     def print_best_score(self):
+        """ Print only the best score """
         pass
 
     def print_all_scores(self):
+        """ Print entire dataframe """
         print(self.score_df)
